2ee3579c0fb1db9735000acf913e4d0af4588aba
galangptrm <galang.ptrm@gmail.com>
2019-10-29 18:26:38 +0700

    Initial Submission 2

--- news-reader/subs.sh
+++ news-reader/subs.sh
@@ -1,1 +1,7 @@
-curl "https://fcm.googleapis.com/fcm/send/e-9b29KeFFg:APA91bEfZsT5xminwoAtxyv0DQsTCFbqN2eYkMyRjQk7OSCy2-pF9eK4PfYTA0OSyx_CVVA3tur_MajUw8pcJ4vYHXVRBn0nIg28Ydvdp-P1rOFpj-ezQx30ZJPo-aDyClW8k-WXzwQm" --request POST --header "TTL: 60" --header "Content-Length: 0" --header "Authorization: key=AAAAhZPWl5s:APA91bH2-4c67ON3zho6bUmO-BOXyUX72U7JskFD9cBScdxbdet_kj_fatJLbua0KAgC9sJ69-atW7ivNt8v8ODCfqBwGHoYT-FVMUv7LisPr7AthQjQsGtobrfLnsFDB3kwdqHWjYfE"
+curl "https://fcm.googleapis.com/fcm/send/dLhJoTxZjUM:APA91bFXq-bng68hJ9RYLwV0WV4eZ75iSGHfZe_iLVsqrrKjuFQnmnQlDpY24GLIXAbABtUygNn-boWGp2CdnhAvrKVZp6uDDYfNhhDCkeB1qaLqPRfSivhpiShSzgJwT1zFiDLqs0Xf" --request POST --header "TTL: 60" --header "Content-Length: 0" --header "Authorization: key=AAAAhZPWl5s:APA91bHNoDxln_dFFJoHCKcV13EOk3SwBamSnUB6vkF6xw4HtwcLm78BDgwwnjkoLI0V56npk-C6eLT9B5o7ymLvNCE8nWA3o5LerEo_NAYPMXA_j5UZTgr3pY9yPBoOImpGcjcKhHDz"
+
+# Public Key
+# https://fcm.googleapis.com/fcm/send/eHCyK7eCic8:APA91bEqyFh6iT52e8-C_gbfLhA8HI11RLi1pLwU1j2Mgne9S55h7XsjaJxdOt0CQitINP8pnH39Y2-WQzUSRPze7MpTEMYDihQ2BVCnoX3sVkGXsNnDSxjZY3t6uMLk3ubfyL-4uw8W
+
+# Private Key
+# CZPYbl9SXJbyt8oIqyjw42Tjzp7YP0RYCe-o5hoPGKs
